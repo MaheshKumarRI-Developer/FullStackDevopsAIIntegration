556ff595cb65da4afcb277e44a1f907beb938bbe
MaheshKumarRI-Developer <MaheshKumarRI-Developer>
2026-06-11 23:43:31 +0530

    completed the project ready for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,9 +5,10 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB connected");
+    return true;
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
-    process.exit(1);
+    return false;
   }
 };
 
